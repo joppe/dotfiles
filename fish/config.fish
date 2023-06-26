9de8881706f6e0e1806d88b11bc722a5e5054668
joppe <joppe@apestaartje.info>
2023-06-26 16:13:35 +0200

    feat(fish): added ll alias

--- fish/config.fish
+++ fish/config.fish
@@ -4,7 +4,11 @@ end
 
 status --is-interactive; and source (nodenv init -|psub)
 
+set -gx EDITOR nvim
+
 starship init fish | source
 
 alias l="exa --group-directories-first --icons -F -T -L=1"
 alias ll="ls -la"
+
+zoxide init fish | source
