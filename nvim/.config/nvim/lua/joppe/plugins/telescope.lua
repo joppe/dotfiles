907c595371e0a0095f09adb35cab2613b012ea78
joppe <joppe@apestaartje.info>
2024-06-24 21:09:04 +0200

    feat: added telescope plugins to search for text in files

--- nvim/.config/nvim/lua/joppe/plugins/telescope.lua
+++ nvim/.config/nvim/lua/joppe/plugins/telescope.lua
@@ -3,6 +3,7 @@ return {
   branch = "0.1.x",
   dependencies = {
     { "nvim-lua/plenary.nvim" },
+    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
     {
       "nvim-telescope/telescope-live-grep-args.nvim",
       verion = "^1.0.0",
@@ -11,11 +12,26 @@ return {
   config = function()
     local builtin = require("telescope.builtin")
 
+    require("telescope").setup({
+      extensions = {
+        fzf = {
+          fuzzy = true,
+          override_generic_sorter = true,
+          override_file_sorter = true,
+          case_mode = "smart_case",
+        },
+      },
+    })
+
+    require("telescope").load_extension("fzf")
     require("telescope").load_extension("live_grep_args")
 
     vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
     vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
     vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
     vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+    vim.keymap.set("n", "<leader>fs", function()
+      require("telescope").extensions.live_grep_args.live_grep_args()
+    end, {})
   end,
 }
