df40d0305d1df21ac4b9640875ba99f3a5ca478e
joppe <joppe@apestaartje.info>
2024-08-19 12:45:46 +0200

    feat(wezterm): added config

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -7,7 +7,12 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.color_scheme = "Catppuccin Mocha"
+config.color_scheme = "catppuccin-mocha"
+
+config.font = wezterm.font("JetBrains Mono")
+config.window_background_opacity = 0.9
+config.text_background_opacity = 0.8
+config.hide_tab_bar_if_only_one_tab = true
 
 -- and finally, return the configuration to wezterm
 return config
