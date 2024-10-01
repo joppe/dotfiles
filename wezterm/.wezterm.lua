b6c62eb0fabf378e6c1c33bce110fa60405549ae
joppe <joppe@apestaartje.info>
2024-10-01 16:40:04 +0200

    wezterm remove alt enter shortcut and set font-size

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -4,12 +4,19 @@ local wezterm = require("wezterm")
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
--- This is where you actually apply your config choices
+-- This key binding interferes with neovim copilot
+config.keys = {
+	{
+		mods = "ALT",
+		key = "Enter",
+		action = wezterm.action.DisableDefaultAssignment,
+	},
+}
 
--- For example, changing the color scheme:
 config.color_scheme = "catppuccin-mocha"
 
 config.font = wezterm.font("JetBrains Mono")
+config.font_size = 11.0
 config.window_background_opacity = 0.9
 config.text_background_opacity = 0.8
 config.hide_tab_bar_if_only_one_tab = true
