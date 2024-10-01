c273c37dea2b2c4b57cb97ab029a6c5997a91909
joppe <joppe@apestaartje.info>
2024-10-01 16:37:06 +0200

    mason config use ts_ls instead of tsserver and added gopls

--- nvim/.config/nvim/lua/joppe/plugins/lsp/mason.lua
+++ nvim/.config/nvim/lua/joppe/plugins/lsp/mason.lua
@@ -27,7 +27,7 @@ return {
     mason_lspconfig.setup({
       -- list of servers for mason to install
       ensure_installed = {
-        "tsserver",
+        "ts_ls",
         "html",
         "cssls",
         "tailwindcss",
@@ -36,6 +36,7 @@ return {
         "graphql",
         "emmet_ls",
         "pyright",
+        "gopls",
       },
     })
 
