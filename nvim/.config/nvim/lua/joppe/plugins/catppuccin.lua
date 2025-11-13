00d10784c0848ae7da11fc4462d958e7342ea7fc
joppe <joppe@apestaartje.info>
2025-11-13 19:00:43 +0100

    feat: neovim explicitly set mocha palette

--- nvim/.config/nvim/lua/joppe/plugins/catppuccin.lua
+++ nvim/.config/nvim/lua/joppe/plugins/catppuccin.lua
@@ -3,7 +3,9 @@ local config = {
   name = "catppuccin",
   priority = 1000,
   config = function()
-    require("catppuccin").setup()
+    require("catppuccin").setup({
+      flavour = "mocha", -- latte, frappe, macchiato, mocha
+    })
 
     vim.cmd.colorscheme("catppuccin")
   end,
